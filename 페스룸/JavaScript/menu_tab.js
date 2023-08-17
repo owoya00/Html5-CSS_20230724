391820ec7cdd44d61c568ef1f30fce6b2b20e0a1
owoya00 <owoya00@gmail.com>
2023-08-17 18:07:23 +0900

    first commit

--- 페스룸/JavaScript/menu_tab.js
+++ 페스룸/JavaScript/menu_tab.js
@@ -1,5 +1,9 @@
 let content = document.querySelector(".all_menupro");
 let btn = document.querySelectorAll(".tablink");
+let pro = document.querySelectorAll(".product_info > ul > li");
+let ht = pro[0].offsetHeight;
+console.log(ht);
+document.querySelector(".product_info").style.height = ht + "px";
 content.style.backgroundImage = "url(images2/slide1.gif)";
 btn.forEach(function (item, keys) {
   console.log("item :" + item + "\n" + "keys : " + keys);
@@ -9,12 +13,18 @@ btn.forEach(function (item, keys) {
     function others(item) {
       return item !== j;
     }
-    console.log("j : " + j);
+    let otherItems = Array.from(pro).filter(others);
+    otherItems.forEach(function (item) {
+      item.style.opacity = "0";
+    });
+    pro[j].style.opacity = "1";
+    // console.log("j : " + j);
     content.style.backgroundImage = "url(images2/slide" + (j + 1) + ".gif)";
     let otherItem = Array.from(btn).filter(others);
     otherItem.forEach(function (item) {
       item.style.color = "#000";
     });
+
     this.style.color = "red";
   };
 });
