46c293a0312293044c9c13cb533702471b0ceb8c
owoya00 <owoya00@gmail.com>
2023-10-10 12:37:21 +0900

    first commit

--- converse/js/main.js
+++ converse/js/main.js
@@ -17,4 +17,39 @@ $(function () {
       prevEl: ".swiper-button-prev",
     },
   });
+  let menu = true;
+  $(".search").click(function () {
+    if (menu) {
+      $(".nav").css("top", "0");
+      $(".search").css("width", "60px");
+      $(".search p").css("display", "none");
+      $(".search>img").css("display", "none");
+      $(".top_btn_on").css("display", "block");
+
+      $(".SEARCH_BOX").css("left", "0");
+    } else {
+      $(".nav").css("top", "75px");
+      $(".search").css("width", "250px");
+      $(".search p").css("display", "block");
+      $(".search>img").css("display", "block");
+      $(".top_btn_on").css("display", "none");
+      $(".SEARCH_BOX").css("left", "-100%");
+    }
+    menu = !menu;
+  });
+
+  $(window).scroll(function () {
+    let ht = $(window).scrollTop();
+    if (ht >= 70) {
+      $(".search").css("width", "60px");
+      $(".search p").css("display", "none");
+      $(".search>img").css("display", "none");
+      $(".top_btn_on").css("display", "block");
+    } else {
+      $(".search").css("width", "250px");
+      $(".search p").css("display", "block");
+      $(".search>img").css("display", "block");
+      $(".top_btn_on").css("display", "none");
+    }
+  });
 });
